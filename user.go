a6f8b30028209c39cfdd03e6c4af275cab0a2cfc
dmr <dmitryd.prog@gmail.com>
2016-03-28 12:35:48 +0700

    Add user model

---
+++ user.go
@@ -0,0 +1,12 @@
+package models
+
+import (
+	"gopkg.in/mgo.v2/bson"
+)
+
+type User struct {
+	Id       bson.ObjectId `json:"id" bson:"_id"`
+	Login    string        `json:"login" bson:"login"`
+	Password string        `json:"password" bson:"password"`
+	Email    string        `json:"email" bson:"email"`
+}
