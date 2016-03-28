b97d25b154fa734f1d7c2bc5ed1ed622981d6ca1
Dmitry Dubina <dmitryd.prog@gmail.com>
2016-03-29 01:38:31 +0700

    Add user model implement Stringer

--- models/user.go
+++ models/user.go
@@ -1,7 +1,13 @@
 package models
 
+import "fmt"
+
 type User struct {
 	Login    string `json:"login" bson:"login"`
 	Password string `json:"password" bson:"password"`
 	Email    string `json:"email" bson:"email"`
+}
+
+func (u User) String() string {
+	return fmt.Sprintf("Login: %v\tPassword: %v\tEmail: %v\n", u.Login, u.Password, u.Email)
 }
