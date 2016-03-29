c0b62bed99fb5aaa9e0618e7d67160f82a66a5eb
dmr <dmitryd.prog@gmail.com>
2016-03-29 11:44:50 +0700

    Added Find method for user model.

--- models/user.go
+++ models/user.go
@@ -1,6 +1,9 @@
 package models
 
-import "fmt"
+import (
+	"fmt"
+	"gopkg.in/mgo.v2"
+)
 
 type User struct {
 	Login    string `json:"login" bson:"login"`
@@ -8,6 +11,19 @@ type User struct {
 	Email    string `json:"email" bson:"email"`
 }
 
+// Implement stringer
 func (u User) String() string {
 	return fmt.Sprintf("Login: %v\tPassword: %v\tEmail: %v\n", u.Login, u.Password, u.Email)
+}
+
+func (u User) Find(c *mgo.Collection) *mgo.Query {
+	find_user_query := `$or: [
+        {"login": %v},
+        {"email": %v}
+    ]`
+
+	query := fmt.Sprintf(find_user_query, u.Login, u.Email)
+	collect := c.Find(query)
+
+	return collect
 }
