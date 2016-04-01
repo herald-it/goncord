fc0f858993f18eb7a775f062cbe6d96a897c8c72
dmr <dmitryd.prog@gmail.com>
2016-04-01 14:36:00 +0700

    Add login jwt.

--- controllers/user.go
+++ controllers/user.go
@@ -1,13 +1,19 @@
 package controllers
 
 import (
+	//"encoding/base64"
 	"encoding/hex"
+	"io/ioutil"
+	"log"
 	"net/http"
+	"strings"
+	"time"
 
 	"github.com/herald-it/goncord/models"
 	"github.com/herald-it/goncord/pwd_hash"
 	"github.com/herald-it/goncord/utils"
 
+	"github.com/dgrijalva/jwt-go"
 	"github.com/julienschmidt/httprouter"
 	"gopkg.in/mgo.v2"
 )
@@ -27,7 +33,78 @@ func NewUserController(s *mgo.Session) *UserController {
 func (uc UserController) LoginUser(
 	w http.ResponseWriter,
 	r *http.Request,
-	ps httprouter.Param) {
+	ps httprouter.Params) {
+
+	collect := uc.GetDB().C("users")
+
+	err := r.ParseForm()
+	utils.LogError(err)
+
+	if err != nil {
+		http.Error(w, http.StatusText(http.StatusBadRequest), http.StatusBadRequest)
+	}
+
+	usr := new(models.User)
+	utils.Fill(usr, r.PostForm)
+
+	usr.Password = hex.EncodeToString(pwd_hash.Sum([]byte(usr.Password)))
+
+	n, err := usr.FindWithPwd(collect).Count()
+	utils.LogError(err)
+
+	if err != nil {
+		http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+		return
+	}
+
+	if n != 1 {
+		http.Error(w, http.StatusText(http.StatusBadRequest), http.StatusBadRequest)
+		w.Write([]byte("User not found!"))
+		return
+	}
+
+	file_pub_key, err := ioutil.ReadFile("public.key")
+	utils.LogError(err)
+	rsa_pub_key, err := jwt.ParseRSAPublicKeyFromPEM(file_pub_key)
+	_ = rsa_pub_key
+	utils.LogError(err)
+
+	file_pri_key, err := ioutil.ReadFile("private.key")
+	utils.LogError(err)
+	rsa_pri_key, err := jwt.ParseRSAPrivateKeyFromPEM(file_pri_key)
+	_ = rsa_pri_key
+	utils.LogError(err)
+
+	token := jwt.New(jwt.SigningMethodRS256)
+	token.Claims["email"] = usr.Email
+	token.Claims["login"] = usr.Login
+	token.Claims["iat"] = time.Now().Unix()
+
+	log.Print(token.Header)
+	log.Print(token.Claims)
+
+	token.Method = jwt.GetSigningMethod("RS256")
+
+	//base_token, _ := token.SigningString(pub_key)
+	raw_token_strng, _ := token.SigningString()
+	log.Print("Raw token: " + raw_token_strng)
+
+	sign, err := token.Method.Sign(raw_token_strng, rsa_pri_key)
+	utils.LogError(err)
+
+	log.Print("Sign: " + sign)
+
+	err = token.Method.Verify(strings.Join(strings.Split(raw_token_strng, ".")[0:2], "."), sign, rsa_pub_key)
+	utils.LogError(err)
+	//m := jwt.GetSigningMethod("RS256")
+	//m.Sign(signingString, key)
+
+	// arr := strings.Split(base_token, ".")
+
+	// for _, e := range arr {
+	// 	src, _ := base64.StdEncoding.DecodeString(e)
+	// 	log.Print(string(src))
+	// }
 }
 
 func (uc UserController) RegisterUser(
@@ -57,7 +134,7 @@ func (uc UserController) RegisterUser(
 		return
 	}
 
-	if n != 0 {
+	if n == 1 {
 		http.Error(w, http.StatusText(http.StatusNotAcceptable), http.StatusNotAcceptable)
 		w.Write([]byte("User already exist"))
 		return
