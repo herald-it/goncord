2f0df1215939e0eeee7b702ffd8c4df00b94461a
dmr <dmitryd.prog@gmail.com>
2016-04-04 14:19:29 +0700

    Change user models, added bson id field. Add generate new json web token method.

--- models/user.go
+++ models/user.go
@@ -1,42 +1,40 @@
 package models
 
 import (
+	"crypto/rsa"
 	"fmt"
-	"gopkg.in/mgo.v2"
+	"github.com/dgrijalva/jwt-go"
+	"gopkg.in/mgo.v2/bson"
+	"time"
 )
 
 type User struct {
-	Login    string `json:"login" bson:"login"`
-	Password string `json:"password" bson:"password"`
-	Email    string `json:"email" bson:"email"`
+	Id       bson.ObjectId `json:"_id" bson:"_id,omitempty" schema:"-"`
+	Login    string        `json:"login" bson:"login"`
+	Password string        `json:"password" bson:"password"`
+	Email    string        `json:"email" bson:"email"`
 }
 
 // Implement stringer
 func (u User) String() string {
-	return fmt.Sprintf("Login: %v\tPassword: %v\tEmail: %v\n", u.Login, u.Password, u.Email)
+	return fmt.Sprintf("Id: %v\tLogin: %v\tPassword: %v\tEmail: %v\n", u.Id, u.Login, u.Password, u.Email)
 }
 
-func (u *User) Find(c *mgo.Collection) *mgo.Query {
-	find_user_query := `$or: [
-        {"login": %v},
-        {"email": %v}
-    ]`
+func (u User) NewToken(pk *rsa.PrivateKey) (string, error) {
+	token := jwt.New(jwt.SigningMethodRS256)
+	token.Claims["email"] = u.Email
+	token.Claims["login"] = u.Login
+	token.Claims["iat"] = time.Now().Unix()
 
-	query := fmt.Sprintf(find_user_query, u.Login, u.Email)
-	collect := c.Find(query)
+	raw_token_strng, err := token.SigningString()
+	if err != nil {
+		return "", err
+	}
 
-	return collect
-}
+	sign, err := token.Method.Sign(raw_token_strng, pk)
+	if err != nil {
+		return "", err
+	}
 
-func (u *User) FindWithPwd(c *mgo.Collection) *mgo.Query {
-	find_user_query := `$or: [
-        {"login": %v},
-        {"email": %v},
-        {"password": %v}
-    ]`
-
-	query := fmt.Sprintf(find_user_query, u.Login, u.Email, u.Password)
-	collect := c.Find(query)
-
-	return collect
+	return raw_token_strng + "." + sign, nil
 }
