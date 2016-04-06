bad5c39c97cc716bc142675490a897390c9670a3
dmr <dmitryd.prog@gmail.com>
2016-04-06 14:17:19 +0700

    Change test fraemwork on Convey.

--- models/dump_token_test.go
+++ models/dump_token_test.go
@@ -8,25 +8,26 @@ import (
 )
 
 func TestNewDumpTokenModel(t *testing.T) {
-	dump_token := &models.DumpToken{}
-
 	Convey("Create new dump token", t, func() {
+		dump_token := &models.DumpToken{}
 		So(dump_token, ShouldNotBeNil)
 	})
 }
 
 func TestJsonDumpTokenModel(t *testing.T) {
-	dump_token := models.DumpToken{
-		Token: "my_secret_token"}
+	Convey("Model to json format", t, func() {
+		dump_token := models.DumpToken{
+			Token: "my_secret_token"}
 
-	const str = `{"token":"my_secret_token"}`
-	b, e := json.Marshal(&dump_token)
+		const str = `{"token":"my_secret_token"}`
+		b, e := json.Marshal(&dump_token)
 
-	Convey("Marshal struct to json", t, func() {
-		So(e, ShouldBeNil)
-	})
+		Convey("Marshal struct to json", func() {
+			So(e, ShouldBeNil)
+		})
 
-	Convey("Test correct jsonify", t, func() {
-		So(string(b), ShouldEqual, str)
+		Convey("Test correct jsonify", func() {
+			So(string(b), ShouldEqual, str)
+		})
 	})
 }
