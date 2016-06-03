9163b23ca181030c495f8ba84d37f977ee8d2c49
diman3241 <dmitryd.prog@gmail.com>
2016-06-03 11:37:08 +0700

    Add opportunity not send post jwt, will be from cookie

--- controllers/service.go
+++ controllers/service.go
@@ -2,9 +2,10 @@ package controllers
 
 import (
 	"encoding/json"
-	"github.com/dgrijalva/jwt-go"
 	"net/http"
 	"time"
+
+	"github.com/dgrijalva/jwt-go"
 
 	"github.com/julienschmidt/httprouter"
 	"gopkg.in/mgo.v2"
@@ -32,47 +33,52 @@ func (sc ServiceController) IsValid(
 	ps httprouter.Params) *HttpError {
 
 	collect := sc.GetDB().C(models.Set.Database.TokenTable)
+	token := &models.DumpToken{}
 
-	if err := r.ParseForm(); err != nil {
-		return &HttpError{err, "Post form can not be parsed.", 500}
-	}
+	jwtCookie, cookieErr := r.Cookie("jwt")
+	if cookieErr != nil {
+		if err := r.ParseForm(); err != nil {
+			return &HttpError{err, "Post form can not be parsed.", 500}
+		}
 
-	token := new(models.DumpToken)
-	if err := Fill(token, r.PostForm); err != nil {
-		return &HttpError{err, "Post form is not consistent with structure.", 500}
+		if err := Fill(token, r.PostForm); err != nil {
+			return &HttpError{err, "Post form is not consistent with structure.", 500}
+		}
+	} else {
+		token.Token = jwtCookie.Value
 	}
 
 	if token.Token == "" {
 		return &HttpError{nil, "Invalid token value.", 500}
 	}
 
-	find_dump_token, err := querying.FindDumpToken(token, collect)
-	if err != nil || find_dump_token == nil {
+	findDumpToken, err := querying.FindDumpToken(token, collect)
+	if err != nil || findDumpToken == nil {
 		return &HttpError{err, "Token not found.", 500}
 	}
 
-	token_pars, err := jwt.Parse(find_dump_token.Token, nil)
-	life_time := token_pars.Claims["iat"]
+	tokenPars, err := jwt.Parse(findDumpToken.Token, nil)
+	lifeTime := tokenPars.Claims["iat"]
 
-	time_span := time.Now().Unix() - int64(life_time.(float64))
-	if time_span > (7 * 24 * 60 * 60) {
-		collect.Remove(find_dump_token)
+	timeSpan := time.Now().Unix() - int64(lifeTime.(float64))
+	if timeSpan > (7 * 24 * 60 * 60) {
+		collect.Remove(findDumpToken)
 		return &HttpError{nil, "Time token life has expired.", 500}
 	}
 
 	usr := new(models.User)
-	usr.Id = find_dump_token.UserId
+	usr.Id = findDumpToken.UserId
 
-	find_usr, err := querying.FindUser(usr, sc.GetDB().C(models.Set.Database.UserTable))
+	findUsr, err := querying.FindUser(usr, sc.GetDB().C(models.Set.Database.UserTable))
 	if err != nil {
 		return &HttpError{err, "User not found.", 500}
 	}
 
-	json_usr, err := json.Marshal(find_usr)
+	jsonUsr, err := json.Marshal(findUsr)
 	if err != nil {
 		return &HttpError{err, "User can not convert to json.", 500}
 	}
 
-	w.Write(json_usr)
+	w.Write(jsonUsr)
 	return nil
 }
