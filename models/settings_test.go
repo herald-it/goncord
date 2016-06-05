7b78b3da743c53a3387063f5a7b66e0f8cd478ba
Dmitry Dubina <dmitryd.prog@gmail.com>
2016-06-05 16:45:34 +0700

    Fix bad test.

--- models/settings_test.go
+++ models/settings_test.go
@@ -1,16 +1,17 @@
 package models_test
 
 import (
+	"testing"
+
 	"github.com/herald-it/goncord/models"
 	. "github.com/smartystreets/goconvey/convey"
-	"testing"
 )
 
 func TestLoadSettings(t *testing.T) {
 	Convey("Load settings from test", t, func() {
 		err := models.LoadSettings()
 
-		So(err, ShouldNotBeNil)
+		So(err, ShouldBeNil)
 		So(models.Set.Database, ShouldNotBeNil)
 		So(models.Set.Database.DbName, ShouldNotBeNil)
 		So(models.Set.Database.Host, ShouldNotBeNil)
