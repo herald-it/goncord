867b4597952281e34ad836669aad7a65ebd27358
Dmitry Dubina <dmitryd.prog@gmail.com>
2016-06-05 17:00:29 +0700

    Add new test section Rename var

--- utils/pwd_hash/pwd_hash_test.go
+++ utils/pwd_hash/pwd_hash_test.go
@@ -3,53 +3,58 @@ package pwd_hash_test
 import (
 	"crypto/md5"
 	"crypto/sha1"
-	"github.com/herald-it/goncord/utils/pwd_hash"
-	. "github.com/smartystreets/goconvey/convey"
 	"testing"
+
+	pwdHash "github.com/herald-it/goncord/utils/pwd_hash"
+	. "github.com/smartystreets/goconvey/convey"
 )
 
 const pwd = "my_super_crypto_password"
 
 func TestCorrectHashFunc(t *testing.T) {
 	Convey("Test correct hash func", t, func() {
-		sha1_sum := sha1.Sum([]byte(pwd))
-		vanilla_sum := md5.Sum(sha1_sum[:])
-		vanilla_sum_slice := vanilla_sum[:]
+		sha1Sum := sha1.Sum([]byte(pwd))
+		vanillaSum := md5.Sum(sha1Sum[:])
+		vanillaSumSlice := vanillaSum[:]
 
-		my_pwd_hash := pwd_hash.Sum([]byte(pwd))
+		myPwdHash := pwdHash.Sum([]byte(pwd))
 
 		Convey("Equal self hash and original hash", func() {
-			So(string(vanilla_sum_slice), ShouldEqual, string(my_pwd_hash))
+			So(string(vanillaSumSlice), ShouldEqual, string(myPwdHash))
 		})
 
-		hash := pwd_hash.New()
+		hash := pwdHash.New()
 		hash.Write([]byte(pwd))
 
 		Convey("Test hash sum method", func() {
 			hash_sum := hash.Sum(nil)
-			So(string(hash_sum), ShouldEqual, string(vanilla_sum_slice))
+			So(string(hash_sum), ShouldEqual, string(vanillaSumSlice))
 		})
 	})
 }
 
 func TestCorrectWriteFunc(t *testing.T) {
 	Convey("test correct write function", t, func() {
-		sha1_sum := sha1.Sum([]byte(pwd))
-		vanilla_sum := md5.Sum(sha1_sum[:])
-		vanilla_sum_slice := vanilla_sum[:]
+		sha1Sum := sha1.Sum([]byte(pwd))
+		vanillaSum := md5.Sum(sha1Sum[:])
+		vanillaSumSlice := vanillaSum[:]
 
-		hash := pwd_hash.New()
+		hash := pwdHash.New()
 		hash.Write([]byte(pwd))
 
-		hash_sum := hash.Sum(nil)
+		hashSum := hash.Sum(nil)
 
-		So(string(hash_sum), ShouldEqual, string(vanilla_sum_slice))
+		So(string(hashSum), ShouldEqual, string(vanillaSumSlice))
 	})
 }
 
 func TestCorrectData(t *testing.T) {
 	Convey("Test constant data", t, func() {
-		So(pwd_hash.Size, ShouldEqual, 16)
-		So(pwd_hash.BlockSize, ShouldEqual, 64)
+		So(pwdHash.Size, ShouldEqual, 16)
+		So(pwdHash.BlockSize, ShouldEqual, 64)
+
+		tmp := pwdHash.New()
+		So(tmp.Size(), ShouldEqual, 16)
+		So(tmp.BlockSize(), ShouldEqual, 64)
 	})
 }
