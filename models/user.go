545c90961db818e618cc3d8bb145386180fe3eb7
Dmitry Dubina <dmitryd.prog@gmail.com>
2016-06-05 16:43:02 +0700

    Refactoring

--- models/user.go
+++ models/user.go
@@ -3,11 +3,13 @@ package models
 import (
 	"crypto/rsa"
 	"fmt"
+	"time"
+
 	"github.com/dgrijalva/jwt-go"
 	"gopkg.in/mgo.v2/bson"
-	"time"
 )
 
+// User model.
 type User struct {
 	Id       bson.ObjectId `json:"_id,omitempty" bson:"_id,omitempty" schema:"-"`
 	Login    string        `json:"login" bson:"login,omitempty"`
@@ -20,21 +22,23 @@ func (u User) String() string {
 	return fmt.Sprintf("Id: %v\tLogin: %v\tPassword: %v\tEmail: %v\n", u.Id, u.Login, u.Password, u.Email)
 }
 
+// NewToken creates a new token using private key.
+// pk - the private key.
 func (u User) NewToken(pk *rsa.PrivateKey) (string, error) {
 	token := jwt.New(jwt.SigningMethodRS256)
 	token.Claims["email"] = u.Email
 	token.Claims["login"] = u.Login
 	token.Claims["iat"] = time.Now().Unix()
 
-	raw_token_strng, err := token.SigningString()
+	rawTokenStrng, err := token.SigningString()
 	if err != nil {
 		return "", err
 	}
 
-	sign, err := token.Method.Sign(raw_token_strng, pk)
+	sign, err := token.Method.Sign(rawTokenStrng, pk)
 	if err != nil {
 		return "", err
 	}
 
-	return raw_token_strng + "." + sign, nil
+	return rawTokenStrng + "." + sign, nil
 }
