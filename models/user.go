9e1867d4ae650630a6c7ac6dfe7d909853e4e768
diman3241 <dmitryd.prog@gmail.com>
2016-06-06 13:17:54 +0700

    Add scheme tags

--- models/user.go
+++ models/user.go
@@ -12,9 +12,9 @@ import (
 // User model.
 type User struct {
 	Id       bson.ObjectId `json:"_id,omitempty" bson:"_id,omitempty" schema:"-"`
-	Login    string        `json:"login" bson:"login,omitempty"`
-	Password string        `json:"password" bson:"password,omitempty"`
-	Email    string        `json:"email" bson:"email,omitempty"`
+	Login    string        `json:"login" bson:"login,omitempty" scheme:"login"`
+	Password string        `json:"password" bson:"password,omitempty" scheme:"password"`
+	Email    string        `json:"email" bson:"email,omitempty" scheme:"email"`
 }
 
 // Implement stringer
