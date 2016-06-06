93d1d4e972078a0e03929922df7f93a14ce8a5bc
diman3241 <dmitryd.prog@gmail.com>
2016-06-06 16:16:34 +0700

    Add opportunity sets required fields.

--- utils/form_filler.go
+++ utils/form_filler.go
@@ -1,6 +1,7 @@
 package utils
 
 import (
+	"errors"
 	"fmt"
 	"net/url"
 	"reflect"
@@ -8,19 +9,25 @@ import (
 	"strings"
 )
 
-func Fill(i interface{}, form url.Values) error {
-	// decoder := schema.NewDecoder()
-
-	loadModel(i, form)
-	return nil
-}
-
-func loadModel(obj interface{}, m url.Values) {
+// Fill fills the structure with data from a form post.
+// obj - the object to be filled.
+// m - post form.
+// required - the field which should be in post form, in
+// if they are not, the structure is not filled
+// and return the error "Required fields not found.".
+func Fill(obj interface{}, m url.Values, required ...string) error {
 	defer func() {
 		if e := recover(); e != nil {
-			fmt.Printf("Panic! %v\n", e)
+			panic(e)
 		}
 	}()
+
+	for _, reqField := range required {
+		_, exist := m[reqField]
+		if !exist {
+			return errors.New("Required fields not found.")
+		}
+	}
 
 	val := reflect.ValueOf(obj)
 	if val.Kind() == reflect.Ptr {
@@ -28,31 +35,37 @@ func loadModel(obj interface{}, m url.Values) {
 	}
 
 	for k, v := range m {
-		if f := val.FieldByName(strings.Title(k)); f.IsValid() {
-			if f.CanSet() {
+		var f reflect.Value
 
-				switch f.Type().Kind() {
-				case reflect.Int:
-					if i, e := strconv.ParseInt(v[0], 0, 0); e == nil {
-						f.SetInt(i)
-					} else {
-						fmt.Printf("Could not set int value of %s: %s\n", k, e)
-					}
-				case reflect.Float64:
-					if fl, e := strconv.ParseFloat(v[0], 0); e == nil {
-						f.SetFloat(fl)
-					} else {
-						fmt.Printf("Could not set float64 value of %s: %s\n", k, e)
-					}
-				case reflect.String:
-					f.SetString(v[0])
+		if f = val.FieldByName(strings.Title(k)); !f.IsValid() {
+			continue
+		}
 
-				default:
-					fmt.Printf("Unsupported format %v for field %s\n", f.Type().Kind(), k)
-				}
+		if !f.CanSet() {
+			fmt.Printf("Key '%s' cannot be set\n", k)
+			continue
+		}
+
+		switch f.Type().Kind() {
+		case reflect.Int:
+			if i, e := strconv.ParseInt(v[0], 0, 0); e == nil {
+				f.SetInt(i)
 			} else {
-				fmt.Printf("Key '%s' cannot be set\n", k)
+				fmt.Printf("Could not set int value of %s: %s\n", k, e)
 			}
+		case reflect.Float64:
+			if fl, e := strconv.ParseFloat(v[0], 0); e == nil {
+				f.SetFloat(fl)
+			} else {
+				fmt.Printf("Could not set float64 value of %s: %s\n", k, e)
+			}
+		case reflect.String:
+			f.SetString(v[0])
+
+		default:
+			fmt.Printf("Unsupported format %v for field %s\n", f.Type().Kind(), k)
 		}
 	}
+
+	return nil
 }
