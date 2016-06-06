ff3c9ac539cf6cc6604fd0b9d6309e0361c343a9
diman3241 <dmitryd.prog@gmail.com>
2016-06-06 13:17:35 +0700

    Add find by ID method

--- utils/querying/find.go
+++ utils/querying/find.go
@@ -8,12 +8,33 @@ import (
 	"gopkg.in/mgo.v2/bson"
 )
 
-func FindUser(obj *User, c *mgo.Collection) (*User, error) {
+func FindUserID(obj *User, c *mgo.Collection) (*User, error) {
 	var results []User
 
 	err := c.Find(
 		bson.M{"$or": []bson.M{
 			bson.M{"_id": obj.Id},
+		}}).All(&results)
+	if err != nil {
+		return nil, err
+	}
+
+	if len(results) > 1 {
+		return nil, errors.New("Find user returned more 1 record.")
+	}
+
+	if len(results) == 0 {
+		return nil, nil
+	}
+
+	return &results[0], nil
+}
+
+func FindUser(obj *User, c *mgo.Collection) (*User, error) {
+	var results []User
+
+	err := c.Find(
+		bson.M{"$or": []bson.M{
 			bson.M{"login": obj.Login},
 			bson.M{"email": obj.Password},
 		}}).All(&results)
