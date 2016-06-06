c4636d282cfe81be15f0aa66f0031eda70e58fb5
diman3241 <dmitryd.prog@gmail.com>
2016-06-06 14:49:29 +0700

    Del gorilla web toolkit parse form

--- models/user.go
+++ models/user.go
@@ -11,10 +11,10 @@ import (
 
 // User model.
 type User struct {
-	Id       bson.ObjectId `json:"_id,omitempty" bson:"_id,omitempty" schema:"-"`
-	Login    string        `json:"login" bson:"login,omitempty" scheme:"login"`
-	Password string        `json:"password" bson:"password,omitempty" scheme:"password"`
-	Email    string        `json:"email" bson:"email,omitempty" scheme:"email"`
+	Id       bson.ObjectId `json:"_id,omitempty" bson:"_id,omitempty"`
+	Login    string        `json:"login" bson:"login,omitempty"`
+	Password string        `json:"password" bson:"password,omitempty"`
+	Email    string        `json:"email" bson:"email,omitempty"`
 }
 
 // Implement stringer
