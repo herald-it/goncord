00e1145ef5f6f46a2f0fe62e3c33bdf297cafa8b
diman3241 <dmitryd.prog@gmail.com>
2016-06-16 12:47:09 +0700

    More log inform

--- middleware/logging.go
+++ middleware/logging.go
@@ -12,13 +12,19 @@ import (
 // Prints the request processing time.
 func Logging(next Handle) Handle {
 	return func(w http.ResponseWriter, r *http.Request, p Params) {
-		log.Println("\033[7m\033[1mThe incoming request:\033[0m ", r)
+		log.Println("\033[7m\033[1mIncoming request.\033[0m")
+		log.Printf("\033[7m\033[1mMethod:\033[0m %v", r.Method)
+		log.Printf("\033[7m\033[1mForm:\033[0m %v", r.Form)
+		log.Printf("\033[7m\033[1mHost:\033[0m %v", r.Host)
+		log.Printf("\033[7m\033[1mRemote address:\033[0m %v", r.RemoteAddr)
+		log.Printf("\033[7m\033[1mRequest URI:\033[0m %v", r.RequestURI)
+		log.Printf("\033[7m\033[1mUser agent:\033[0m %v", r.UserAgent())
+		log.Printf("\033[7m\033[1mHeaders:\033[0m %v", r.Header)
 
 		t0 := time.Now()
 		next(w, r, p)
 		t1 := time.Now()
 
-		log.Println("The request has been processed.")
 		log.Printf("\033[7m\033[1mElapsed time: %v\033[0m", t1.Sub(t0))
 	}
 }
--- utils/handler.go
+++ utils/handler.go
@@ -25,7 +25,7 @@ type ErrHandler func(http.ResponseWriter, *http.Request, Params) *HttpError
 func ErrWrap(eh ErrHandler) Handle {
 	return Handle(func(w http.ResponseWriter, r *http.Request, p Params) {
 		if e := eh(w, r, p); e != nil {
-			log.Printf("Error: %v Message: %v Code: %v\n", e.Error, e.Message, e.Code)
+			log.Printf("\033[7m\033[1mError: %v Message: %v Code: %v\033[0m", e.Error, e.Message, e.Code)
 
 			if failureURL := r.URL.Query().Get("failure"); failureURL != "" {
 				http.Redirect(w, r, failureURL, 301)
@@ -33,7 +33,7 @@ func ErrWrap(eh ErrHandler) Handle {
 				http.Error(w, e.Message, e.Code)
 			}
 		} else {
-			log.Printf("Success\n\033[7m\033[1mStatus: %v\033[0m", e.Code)
+			log.Printf("\033[7m\033[1mSuccessfully.\033[0m")
 
 			if successURL := r.URL.Query().Get("success"); successURL != "" {
 				http.Redirect(w, r, successURL, 301)
