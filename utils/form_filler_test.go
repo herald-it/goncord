6a140bcc9d592fbc157a0bb79d2b1e8dec12447b
diman3241 <dmitryd.prog@gmail.com>
2016-08-30 09:58:03 +0700

    Added more coverage in test file. Test FillForm method.

--- utils/form_filler_test.go
+++ utils/form_filler_test.go
@@ -1,10 +1,11 @@
 package utils_test
 
 import (
-	"github.com/herald-it/goncord/utils"
-	. "github.com/smartystreets/goconvey/convey"
 	"net/url"
 	"testing"
+
+	"github.com/herald-it/goncord/utils"
+	. "github.com/smartystreets/goconvey/convey"
 )
 
 type TestStruct struct {
@@ -27,6 +28,54 @@ func TestFormFiller(t *testing.T) {
 		Convey("Correct parse form", func() {
 			So(ts.Field, ShouldEqual, "form_field")
 			So(ts.Field2, ShouldEqual, 1)
+		})
+
+		Convey("Test fill object method", func() {
+			testStruct := struct {
+				Field1 int
+				Field2 string
+				Field3 float64
+			}{}
+
+			err := utils.Fill(
+				&testStruct,
+				url.Values{
+					"Field1": []string{"5"},
+				},
+				"Field1",
+			)
+
+			So(err, ShouldBeNil)
+			So(testStruct.Field1, ShouldEqual, 5)
+
+			err = utils.Fill(
+				&testStruct,
+				url.Values{
+					"Field2": []string{"hello"},
+					"Field3": []string{"3.14"},
+				},
+				"Field1|Field2", "Field3",
+			)
+
+			So(err, ShouldBeNil)
+			So(testStruct.Field1, ShouldEqual, 5)
+			So(testStruct.Field2, ShouldEqual, "hello")
+			So(testStruct.Field3, ShouldEqual, 3.14)
+
+			err = utils.Fill(
+				&testStruct,
+				url.Values{
+					"Field1": []string{"1"},
+					"Field2": []string{"2"},
+					"Field3": []string{"3"},
+				},
+				"Field1|Field2|Field3",
+			)
+
+			So(err, ShouldBeNil)
+			So(testStruct.Field1, ShouldEqual, 1)
+			So(testStruct.Field2, ShouldEqual, "2")
+			So(testStruct.Field3, ShouldEqual, 3.0)
 		})
 	})
 }
